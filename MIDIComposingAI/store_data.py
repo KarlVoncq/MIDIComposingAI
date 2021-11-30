a5009421a9cfdd86389c5d714d315990c2d4ddce
karlvoncq <karl.voncq@gmail.com>
2021-11-30 18:29:37 +0100

    Functions updated

--- MIDIComposingAI/store_data.py
+++ MIDIComposingAI/store_data.py
@@ -4,16 +4,16 @@ import joblib
 
 # Get your path to adl-piano-midi dataset
 # See README
-path_to_dataset = 'YOUR/PATH/TO/DATASET'
 
-# Get all paths to files inside the dataset
-targetPattern = fr"{{path_to_dataset}}/*/*/*/*/*/*.mid"
-midi_files_paths = glob.glob(targetPattern)
+def store_data(path_to_adl_dataset, path_to_pretty_midi_dir):
 
-# Store the pretty_midi dataset
-path_to_pretty_midi_dir = "YOUR/PATH/TO/DIR"
-for file in midi_files_paths:
-    try: # We chose to ignore the few files not working with pretty_midi
-        joblib.dump(pretty_midi.PrettyMIDI(file), f'{path_to_pretty_midi_dir}/{file.split("/")[-1][:-4]}')
-    except:
-        pass
+    # Get all paths to files inside the dataset
+    targetPattern = fr"{path_to_adl_dataset}/*/*/*/*/*.mid"
+    midi_files_paths = glob.glob(targetPattern)
+
+    # Store the pretty_midi dataset
+    for file in midi_files_paths:
+        try: # We chose to ignore the few files not working with pretty_midi
+            joblib.dump(pretty_midi.PrettyMIDI(file), f'{path_to_pretty_midi_dir}/{file.split("/")[-1][:-4]}')
+        except:
+            pass
